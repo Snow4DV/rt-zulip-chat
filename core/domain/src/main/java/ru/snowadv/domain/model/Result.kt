b6030068094c86e9867d1e143ceb50627cccdcba
snow4dv <log.mihail2003@gmail.com>
2024-03-25 12:29:24 +0300

    Refactor Result class

--- core/domain/src/main/java/ru/snowadv/domain/model/Result.kt
+++ core/domain/src/main/java/ru/snowadv/domain/model/Result.kt
@@ -1,7 +1,7 @@
 package ru.snowadv.domain.model
 
-sealed class Result<T>(val data: T?) {
-    class Loading<T>(data: T? = null): Result<T>(data)
-    class Error<T>(val message: String? = null, data: T? = null): Result<T>(data)
-    class Success<T>(data: T? = null): Result<T>(data)
+sealed class Result<T> {
+    class Loading<T>(data: T? = null): Result<T>()
+    class Error<T>(val message: String? = null, data: T? = null): Result<T>()
+    class Success<T>(data: T): Result<T>()
 }
